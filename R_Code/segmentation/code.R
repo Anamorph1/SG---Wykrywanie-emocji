503cce0562c4c50f60b5bcfd075327994c6c5973
omicronns <omicronns@gmail.com>
2015-11-26 17:33:08 +0100

    preparing for different algorithm

--- R_Code/segmentation/code.R
+++ R_Code/segmentation/code.R
@@ -1,44 +1,50 @@
-el70=read.table('equalLoudnes70.txt',header=TRUE)
-s=load.wave('emotions/speech/neutral/f1neu1.wav')
+rm(list = ls())
 
-fftfilt=function(x,Fs,Fl,Fu,ker) {
-  xfft=fft(x)
-  wnlen=length(x)*(Fu-Fl)/Fs
-  wnd=resamp(10^(ker/10),length(ker),wnlen)
-  iFl=floor(Fl*(length(x)-1)/Fs + 1)
-  mask=c(seq(0,0,length.out=iFl-1),wnd,seq(0,0,length.out=length(s)-2*length(wnd)-2*(iFl-1)),rev(wnd),seq(0,0,length.out=iFl-1))
-  return(Re(fft(xfft*mask,inverse=TRUE))/length(x))
-}
+library(audio)
+library(seewave)
+source('iso226.R')
+s=load.wave('emotions/speech/neutral/m2neu1.wav')
 
-convhull=function(x) {
-	imax=which.max(x)
-	for(it in 2:imax) {
-		if(x[it]<x[it-1]) {
-			x[it]=x[it-1]
-		}
-	}
-	for(it in (length(x)-1):(imax+1)) {
-		if(x[it]<x[it+1]) {
-			x[it]=x[it+1]
-		}
-	}
-	return(x)
-}
-
-segmentRecursion=function(venv,th) {
-  venvp=convhull(venv)-venv;
-  imax=which.max(venvp)
-  if(venvp[imax]/venv[imax] > th) {
-    return(c(segmentRecursion(venv[1:imax],th),segmentRecursion(venv[(imax+1):length(venv)],th)+imax))
-  } else {
-    return(imax)
-  }
+envelope=function(v) {
+  vabs=abs(v)
+  vfil=ffilter(vabs,from = 0.000001,to = 12,output='audioSample')
+  vres=resamp(vfil,g=100)
+  dim(vres)=NULL
+  min2=min(vres[vres > 0])
+  vres[vres <= 0]=min2
+  return(vres^0.3)
 }
 
 segmentVoice=function(v) {
-  vfil=resamp(v,g=16000,output='audioSample')
-	vfil=ffilter(v,from=500,to=4000,wn='rectangle',output='audioSample')
-	venv=ffilter(vfil^2,from=0.1,to=12,wn='hamming')
-	dim(venv)=NULL
-	return(segmentRecursion(venv,10^(2/10)))
+  # Prepare to resample (low-pass 8kHz filter)
+  vfil=ffilter(v,to=8000,output='audioSample')
+  
+  # Resample to 16kHz
+  vres=resamp(vfil,g=16000,output='audioSample')
+  
+  # Equal loudness filter (iso226:2003)
+  vequ=ffilter(vres,custom=10^((70-iso226(70,0,8000,512))/10),output='audioSample')
+  
+  # Split to channels
+  vchan=list(ffilter(vequ,16000,150,1000,output='audioSample'),
+             ffilter(vequ,16000,150,3000,output='audioSample'),
+             ffilter(vequ,16000,from=150,output='audioSample'))
+  
+  # Compute normalized envelopes
+  venv8=envelope(vchan[[3]])
+  venvn=list(envelope(vchan[[1]])/venv8,envelope(vchan[[2]])/venv8)
+  names(venvn)=c('envn1','envn3')
+  
+  # Compute onset envelope velocity
+  vdenv=diff(venv8)
+  vdenv[vdenv < 0] = 0
+  
+  # Find os-s and oe-s
+  
+  
+  return(vdenv)
 }
+
+a=segmentVoice(s)
+# plot(seq(1,length(a$v1env),length.out = length(s)),s,type = 'l')
+plot(1:length(a),a,type = 'l')
