03da80fa367014af6c1c6e020369d538bea40e7c
knopers8 <knopers8@op.pl>
2015-12-01 22:38:28 +0100

    Create main.R

---
+++ R_Code/main/main.R
@@ -0,0 +1,22 @@
+rm(list = ls())
+library(audio);
+library(seewave);
+source("svm.R")
+source('Segmentation.R')
+source('compute_params.R')
+
+load('svm_machine');
+
+
+Params_matrix = compute_params("speech/f3sad2.wav") #speech/m4fea1.wav")
+
+vector = matrix(data = 0, nrow = 1, ncol = 28)
+for(i in 1:28){
+  vector[i] = mean(Params_matrix[,i])
+}
+
+pred <- predict_class(svm_machine, vector)
+print(pred)
+
+
+end
